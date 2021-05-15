67c07a0c49a2b6fdf0ffea13f308d6bd3047fde2
ErickCapilla <erickpoblano25@gmail.com>
2021-05-14 23:31:02 -0500

    Fixed bugs

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,9 @@ fecha_btn.addEventListener('click', () => {
 const css = document.getElementById('css')
 const cambiar = document.getElementById('cambiar')
 cambiar.addEventListener('click', () => {
-  if (css.getAttribute('href') === '../css/style.css') {
-    css.href = '../css/style2.css'
+  if (css.getAttribute('href') === './css/style.css') {
+    css.href = './css/style2.css'
   } else {
-    css.href = '../css/style.css'
+    css.href = './css/style.css'
   }
 })
